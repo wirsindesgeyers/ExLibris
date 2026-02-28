e16c4e94e7a83b574c03e27c4bc07b6a04d6853e
wirsindesgeyers <kauanmaiagomes1307@gmail.com>
2026-02-28 16:40:29 -0400

    docs: adds schema description on ReviewRequestDTO

--- src/main/java/com/biblioteca_api/biblioteca/dto/ReviewRequestDTO.java
+++ src/main/java/com/biblioteca_api/biblioteca/dto/ReviewRequestDTO.java
@@ -1,5 +1,6 @@
 package com.biblioteca_api.biblioteca.dto;
 
+import io.swagger.v3.oas.annotations.media.Schema;
 import jakarta.validation.constraints.Max;
 import jakarta.validation.constraints.Min;
 import jakarta.validation.constraints.NotBlank;
@@ -8,11 +9,21 @@ import jakarta.validation.constraints.Size;
 
 public record ReviewRequestDTO(
 
-        @Max(value = 5, message = "Rating cannot be higher than 5 ") @Min(value = 0, message = "Rating cannot be less than 0") @NotNull short rating,
+        @Schema(description = "Avaliação do livro de 0 a 5", example = "5")
+        @Max(value = 5, message = "Rating cannot be higher than 5 ")
+        @Min(value = 0, message = "Rating cannot be less than 0")
+        @NotNull
+        short rating,
 
-        @Size(max = 35, message = "Maximum number of characters (35) exceeded") @NotBlank(message = "title cannot be blank") String title,
+        @Schema(description = "Resumo da avaliação do livro", example = "Excelente leitura!")
+        @Size(max = 35, message = "Maximum number of characters (35) exceeded")
+        @NotBlank(message = "title cannot be blank")
+        String title,
 
-        @Size(max = 300, message = "Maximum number of characters (300) exceeded") @NotBlank(message = "description cannot be blank") String description
+        @Schema(description = "Descrição detalhada da avaliação", example = "O livro possui uma narrativa envolvente e personagens bem construídos.")
+        @Size(max = 300, message = "Maximum number of characters (300) exceeded")
+        @NotBlank(message = "description cannot be blank")
+        String description
 
 ) {
 }

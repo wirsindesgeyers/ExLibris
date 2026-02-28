52f73543dc1930cadba7c3fd12c1e7e17b8df1d3
wirsindesgeyers <kauanmaiagomes1307@gmail.com>
2026-02-28 16:23:24 -0400

    docs: adds schema description on AuthorResponseDTO

--- src/main/java/com/biblioteca_api/biblioteca/dto/AuthorResponseDTO.java
+++ src/main/java/com/biblioteca_api/biblioteca/dto/AuthorResponseDTO.java
@@ -7,11 +7,20 @@ import java.util.List;
 import com.biblioteca_api.biblioteca.entities.Author;
 import com.biblioteca_api.biblioteca.entities.Book;
 
+import io.swagger.v3.oas.annotations.media.Schema;
+
 public record AuthorResponseDTO(
 
+        @Schema(description = "Id do autor de livros", example = "2328")
         Long id,
+
+        @Schema(description = "Data de Nascimento do Autor de livros (yyyy/MM/dd)", example = "1874/05/29")
         LocalDate birthDate,
+
+        @Schema(description = "Nome do Autor de livros", example = "Gilbert Keith Chesterton")
         String name,
+
+        @Schema(description = "Lista de breves informações dos livros do autor", example = "Id, titulo e data de publicação")
         List<BookInfoDTO> bookInfos
 
 ) {
@@ -29,7 +38,18 @@ public record AuthorResponseDTO(
     }
 }
 
-record BookInfoDTO(Long id, String title, LocalDate publishedDate) {
+record BookInfoDTO(
+
+        @Schema(description = "Id do livro")
+        Long id,
+
+        @Schema(description = "Título do livro")
+        String title,
+
+        @Schema(description = "Data de publicação do livro")
+        LocalDate publishedDate
+
+) {
     public BookInfoDTO(Book book) {
         this(book.getId(), book.getTitle(), book.getPublishedDate());
     }

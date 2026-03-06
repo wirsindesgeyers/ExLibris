9e4d59f28d78a2b16b2431eba36f985a193fe732
wirsindesgeyers <kauanmaiagomes1307@gmail.com>
2026-03-06 12:17:01 -0400

    feat: add find by login method

--- src/main/java/com/biblioteca_api/biblioteca/repository/UserRepository.java
+++ src/main/java/com/biblioteca_api/biblioteca/repository/UserRepository.java
@@ -1,9 +1,11 @@
 package com.biblioteca_api.biblioteca.repository;
 
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.security.core.userdetails.UserDetails;
 
 import com.biblioteca_api.biblioteca.entities.User;
 
 public interface UserRepository extends JpaRepository<User, Long> {
 
+    UserDetails findByEmail(String email);
 }

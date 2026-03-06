9f52877ad99546dbbbe0346e93162f0447a62d16
wirsindesgeyers <kauanmaiagomes1307@gmail.com>
2026-03-06 13:56:51 -0400

    feat: add security filter chain initial configs

---
+++ src/main/java/com/biblioteca_api/biblioteca/infra/security/SecurityConfiguration.java
@@ -0,0 +1,32 @@
+package com.biblioteca_api.biblioteca.infra.security;
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.http.HttpMethod;
+import org.springframework.security.config.annotation.method.configuration.EnableMethodSecurity;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.security.config.http.SessionCreationPolicy;
+import org.springframework.security.web.SecurityFilterChain;
+
+@Configuration
+@EnableWebSecurity
+@EnableMethodSecurity
+public class SecurityConfiguration {
+
+    @Bean
+    public SecurityFilterChain SecurityFilterChain(HttpSecurity httpSecurity) {
+
+        return httpSecurity
+                .csrf(csrf -> csrf.disable())
+                .sessionManagement(session -> session.sessionCreationPolicy(SessionCreationPolicy.STATELESS))
+                .authorizeHttpRequests(authorize -> {
+
+                    authorize.requestMatchers(HttpMethod.POST, "/api/auth/**").permitAll();
+
+                    authorize.anyRequest().authenticated();
+
+                })
+                .build();
+    }
+}

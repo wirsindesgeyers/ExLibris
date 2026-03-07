e8886cb76b4e6c29cfe0f833e62df0ffe2d0fc74
wirsindesgeyers <kauanmaiagomes1307@gmail.com>
2026-03-07 00:22:50 -0400

    feat: handling UserAlreadyExistsException globally

---
+++ src/main/java/com/biblioteca_api/biblioteca/infra/exceptions/GlobalExceptionHandler.java
@@ -0,0 +1,44 @@
+package com.biblioteca_api.biblioteca.infra.exceptions;
+
+
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ProblemDetail;
+import org.springframework.web.bind.annotation.ExceptionHandler;
+import org.springframework.web.bind.annotation.RestControllerAdvice;
+
+import java.time.Instant;
+
+@RestControllerAdvice
+public class GlobalExceptionHandler {
+
+
+    @ExceptionHandler(BookAlreadyExistsException.class)
+    public ProblemDetail handleDuplicateBook(BookAlreadyExistsException e) {
+
+        ProblemDetail problemDetail = ProblemDetail.forStatusAndDetail(
+                HttpStatus.CONFLICT, // 409
+                e.getMessage()
+        );
+
+        problemDetail.setTitle("Conflito de Dados");
+        problemDetail.setProperty("timestamp", Instant.now());
+
+        return problemDetail;
+
+    }
+
+    @ExceptionHandler(UserAlreadyExistsException.class)
+    public ProblemDetail handleDuplicateBook(UserAlreadyExistsException e) {
+
+        ProblemDetail problemDetail = ProblemDetail.forStatusAndDetail(
+                HttpStatus.CONFLICT,
+                e.getMessage()
+        );
+
+        problemDetail.setTitle("Conflito de Dados");
+        problemDetail.setProperty("timestamp", Instant.now());
+
+        return problemDetail;
+
+    }
+}

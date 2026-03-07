b1e1442ea7e95195ef8918e06aa5a0d4d99c5c8c
wirsindesgeyers <kauanmaiagomes1307@gmail.com>
2026-03-07 00:29:37 -0400

    feat: add a filter before the filterchain to recover the token and  check if the user is authorized

--- src/main/java/com/biblioteca_api/biblioteca/infra/security/SecurityFilter.java
+++ src/main/java/com/biblioteca_api/biblioteca/infra/security/SecurityFilter.java
@@ -24,11 +24,14 @@ public class SecurityFilter extends OncePerRequestFilter {
     protected void doFilterInternal(HttpServletRequest request, HttpServletResponse response, FilterChain filterChain) throws ServletException, IOException {
         var token = this.recoverToken(request);
 
-        if(token != null){
+        if(token != null) {
             var email = tokenService.validateToken(token);
             UserDetails user = userRepository.findByEmail(email).orElse(null);
 
-            var authentication = new UsernamePasswordAuthenticationToken(user, null, user.getAuthorities());
+            UsernamePasswordAuthenticationToken authentication = null;
+            if (user != null) {
+                authentication = new UsernamePasswordAuthenticationToken(user, null, user.getAuthorities());
+            }
 
             SecurityContextHolder.getContext().setAuthentication(authentication);
         }

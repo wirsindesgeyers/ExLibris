f10aa57d9d45c8a1d28786fe604b8266056fce0a
wirsindesgeyers <kauanmaiagomes1307@gmail.com>
2026-03-07 00:18:53 -0400

    feat: concat username within not found exception in loadUserByUsername

--- src/main/java/com/biblioteca_api/biblioteca/service/AuthorizationService.java
+++ src/main/java/com/biblioteca_api/biblioteca/service/AuthorizationService.java
@@ -16,6 +16,6 @@ public class AuthorizationService implements UserDetailsService {
     @Override
     public UserDetails loadUserByUsername(String username) throws UsernameNotFoundException {
         return repository.findByEmail(username)
-                .orElseThrow(() -> new UsernameNotFoundException("Usuário não encontrado"));
+                .orElseThrow(() -> new UsernameNotFoundException("Usuário não encontrado: "+ username));
     }
 }

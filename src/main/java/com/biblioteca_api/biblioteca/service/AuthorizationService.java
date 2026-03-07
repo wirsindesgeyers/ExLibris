a5bd4a1029d94a441eedbb54a4c5113ba0f78410
wirsindesgeyers <kauanmaiagomes1307@gmail.com>
2026-03-06 23:52:49 -0400

    feat: add loadUserByUsername (UserDetails)

--- src/main/java/com/biblioteca_api/biblioteca/service/AuthorizationService.java
+++ src/main/java/com/biblioteca_api/biblioteca/service/AuthorizationService.java
@@ -1,24 +1,21 @@
 package com.biblioteca_api.biblioteca.service;
 
+import com.biblioteca_api.biblioteca.repository.UserRepository;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.security.core.userdetails.UserDetails;
 import org.springframework.security.core.userdetails.UserDetailsService;
 import org.springframework.security.core.userdetails.UsernameNotFoundException;
 import org.springframework.stereotype.Service;
 
-import com.biblioteca_api.biblioteca.repository.UserRepository;
-
-import lombok.RequiredArgsConstructor;
-
 @Service
-@RequiredArgsConstructor
 public class AuthorizationService implements UserDetailsService {
 
-    private final UserRepository userRepository;
+    @Autowired
+    private UserRepository repository;
 
     @Override
     public UserDetails loadUserByUsername(String username) throws UsernameNotFoundException {
-        return userRepository.findByEmail(username)
-                .orElseThrow(() -> new UsernameNotFoundException("Nenhum autor encontrado com o e-mail: " + username));
+        return repository.findByEmail(username)
+                .orElseThrow(() -> new UsernameNotFoundException("Usuário não encontrado"));
     }
-
 }

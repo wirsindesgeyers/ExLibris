b9cd0db4326617c87593aabc63b8164de2a6083d
wirsindesgeyers <kauanmaiagomes1307@gmail.com>
2026-03-09 20:31:21 -0400

    feat: changed existsbyuseridandbookid signature because the userid is not needed anymore.

--- src/main/java/com/biblioteca_api/biblioteca/repository/ReviewRepository.java
+++ src/main/java/com/biblioteca_api/biblioteca/repository/ReviewRepository.java
@@ -7,14 +7,16 @@ import org.springframework.data.jpa.repository.Query;
 import org.springframework.data.repository.query.Param;
 
 import com.biblioteca_api.biblioteca.entities.Review;
+import com.biblioteca_api.biblioteca.entities.User;
 
 public interface ReviewRepository extends JpaRepository<Review, Long> {
-    boolean existsByUserIdAndBookId(Long userId, Long bookId);
+    boolean existsByUserAndBookId(User user, Long bookId);
 
     List<Review> findByBookId(Long bookId);
 
     List<Review> findByUserId(Long userId);
 
     @Query("SELECT AVG(r.rating) FROM Review r WHERE r.book.id = :bookId")
-    Double getAverageRatingByBookId(@Param("bookId") Long bookId);
+    Double getAverageRatingByBookId(@Param("bookId")
+    Long bookId);
 }
